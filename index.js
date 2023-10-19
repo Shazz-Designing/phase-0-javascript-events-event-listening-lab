5a424c2bfba9ab69e7a17d7d53cd86a26ead3272
sharon <sapot@safaricom.co.ke>
2023-10-19 22:05:42 +0300

    Add code to index.js

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 function addingEventListener() {
-}
+    
+    const input = document.getElementById('input');
+  
+    input.addEventListener('click', function() {
+      alert('I was clicked!');
+    });
+  }
+  
+  addingEventListener(); 
+  
+  function clickAlert() {
+    alert('I was clicked!');
+  }
+  
+  function addingEventListenerWithNamedFunction() {
+    const input = document.getElementById('input');
+    input.addEventListener('click', clickAlert);
+  }
+  
+  addingEventListenerWithNamedFunction(); 
